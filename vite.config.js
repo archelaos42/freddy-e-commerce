1de564e8c2e25ee27d77d761f1d2513d55238a31
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-01 21:48:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
         // port: 3000,
         // https: true,
         hmr: {
-            host: 'localhost',
+            host: '127.0.0.1',
             // port: 3001,
             protocol: 'wss',
             origin: false,
