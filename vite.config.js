81cf09d565a3113ddc4a1838187188776ed608e5
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-01 22:54:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     ],
     server: {
         // port: 3000,
-        // https: true,
+        http: true,
         hmr: {
             host: 'localhost',
             // port: 3001,
@@ -36,7 +36,7 @@ export default defineConfig({
     resolve: {
         preserveSymlinks: true,
         server: {
-            https: true,
+            // https: true,
             origin: 'localhost'
 
         },
