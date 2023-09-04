9df60a0c1171ef4060c67458e72a8d557676c9c7
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-04 19:23:17 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -81,7 +81,7 @@ class PagesController extends Controller
                                          'blue', 'beige', 'grey', 'military', 'pink', 'black',
                                          'vMin', 'vMax',
                                         ), function ($query, $id) {
-                    $query->where('collection_id', '=', $id);
+                    $query->orWhere('collection_id', '=', $id);
                     if(request()->input('length78') === "true"){
                         $query->orWhere('length', '=', '7/8');
                     }
