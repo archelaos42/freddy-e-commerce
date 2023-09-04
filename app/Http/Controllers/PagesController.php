1628cdfe75befef90be122eb2e72471ce09d544f
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-04 20:28:31 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -71,7 +71,6 @@ class PagesController extends Controller
         return Inertia::render('Collection', [
             'collection' => Collection::findOrFail($id),
             'categories' => Category::query()->where('collection_id', '=', $id)->get(),
-            'id' => $id,
             $content = Cart::content(),
                 'selectedView' => 'multi',
                 'count' => $content->count(),
@@ -83,9 +82,7 @@ class PagesController extends Controller
                                          'vMin', 'vMax',
                                         ), function ($query, $id) {
                     // $query->orWhere('collection_id', '=', $id);
-                    if(!request()){
                         $query->where('collection_id', '=', '1');
-                    }
                     if(request()->input('length78') === "true"){
                         $query->orWhere('length', '=', '7/8');
                     }
