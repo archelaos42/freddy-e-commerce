466681a99f08eaaf2b4c85b85a089fce010e0fee
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-05 07:18:57 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -77,23 +77,23 @@ class PagesController extends Controller
                 'selectedView' => 'multi',
                 'count' => $content->count(),
                 'products' => Product::
-                               // query()->
-                 orWhere('collection_id', '=', '1')
+                               query()->
+                 // orWhere('collection_id', '=', '1')
                 ->when(request()->hasAny('length78', 'length34', 'lengthBl', 'lengthS', 'lengthN' ), function ($query, $col) {
                     if(request()->input('length78') === "true"){
-                        $query->orWhere('length', '=', '7/8');
+                        $query->orWhere('length', '=', '7/8')andWhere('collection_id', '=', '1');
                     }
                     if(request()->input('length34') === "true"){
-                        $query->orWhere('length', '=', '3/4');
+                        $query->orWhere('length', '=', '3/4')andWhere('collection_id', '=', '1');
                     }
                     if(request()->input('lengthBl') === "true"){
-                        $query->orWhere('length', '=', 'bicycle length');
+                        $query->orWhere('length', '=', 'bicycle length')andWhere('collection_id', '=', '1');
                     }
                     if(request()->input('lengthS') === "true"){
-                        $query->orWhere('length', '=', 'short');
+                        $query->orWhere('length', '=', 'short')andWhere('collection_id', '=', '1');
                     }
                     if(request()->input('lengthN') === "true"){
-                        $query->orWhere('length', '=', 'normal');
+                        $query->orWhere('length', '=', 'normal')andWhere('collection_id', '=', '1');
                     }
                 })
                 ->when(request()->hasAny('sizeXxs', 'sizeXs', 'sizeS', 'sizeM', 'sizeL', 'sizeXl' ), function ($query) {
