d897fb3c1a63243818666f2e2d9e29c6f6786f23
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-05 02:24:03 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -156,7 +156,7 @@ class PagesController extends Controller
                         $query->where('price', '<=', request()->input('vMax'));
 
                     })
-                   // ->where('collection_id', '=', $id)
+                   ->where('collection_id', '=', $id)
 //                    ->where('price', '<', 'vMax')
 //                    ->where('price', '>', 'vMin')
                 ->paginate(10)
