4fe5a7c81c128bc155495a4c85a5229d938312a3
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-05 02:50:05 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -71,7 +71,7 @@ class PagesController extends Controller
     {
         return Inertia::render('Collection', [
             'collection' => Collection::findOrFail($id),
-            $col = Collection::findOrFail($id),
+            $col = Collection::findOrFail($id)->get(),
                                // dd($col),
             'categories' => Category::query()->where('collection_id', '=', $id)->get(),
             $content = Cart::content(),
