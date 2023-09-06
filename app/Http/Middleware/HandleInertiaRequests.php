19acbf558eb3426621f721915c088fa4ab396524
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-06 21:50:43 +0200

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'csrf' => csrf_token(),
             'flash' => [
                 'message' => session('message')
 //                'message' => fn () => $request->session()->get('message')
