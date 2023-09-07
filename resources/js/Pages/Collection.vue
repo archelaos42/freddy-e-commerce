523fdeb69fdcea87c1f71b35e8f935eb0d0edae2
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-07 20:08:57 +0200

    Update Collection.vue

--- resources/js/Pages/Collection.vue
+++ resources/js/Pages/Collection.vue
@@ -56,7 +56,9 @@
                             class="slider"
                         />
                         <div class="flex">
-                            <input v-model="vMin" type="number" class="input, w-20 [appearance:textfield] [&::-webkit-outer-spin-button]:h-1 [&::-webkit-inner-spin-button]:h-1" />
+                            <input v-model="vMin" type="number" class="input, w-20 
+                                [&::-webkit-outer-spin-button]:my-2
+                                [&::-webkit-inner-spin-button]:my-2" />
                             <div class="flex text-center">Price in Euros</div>
                             <input v-model="vMax" type="number" class="input, w-20"  />
                         </div>
