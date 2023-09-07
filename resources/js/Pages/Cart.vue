efe4815afa633b4c904951ed3b1cddb9797a62c3
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-08 01:14:51 +0200

    Update Cart.vue

--- resources/js/Pages/Cart.vue
+++ resources/js/Pages/Cart.vue
@@ -52,15 +52,6 @@
                                     </svg>
                                 </button>
                             </div>
-                            <div>
-                                <button>
-                                    <svg width="12" height="12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                                         fill-rule="evenodd" clip-rule="evenodd">
-                                        <path
-                                            d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"/>
-                                    </svg>
-                                </button>
-                            </div>
                         </div>
                         <div className="flex">
                             <button>
