b169504682c032405dd20603f702d79b0c87088f
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-07 23:09:52 +0200

    Update HomeImages.vue

--- resources/js/Layouts/HomeImages.vue
+++ resources/js/Layouts/HomeImages.vue
@@ -34,36 +34,31 @@
                 </div>
             </div>
 
-            <div class="text-sm font-normal">
-                <div class="mb-6">
-                    <span>Your e-mail address (claim award points)</span>
-                </div>
-                <div class="flex justify-between border-b-2 border-gray-600 h-10">
-                    <div class="my-auto">
-                        <input id="first_name" placeholder="Your e-mail address" />
-                    </div>
-                    <div class="w-3/12 pl-6">
-                        <button class="flex justify-center font-ropa text-transform: uppercase text-white bg-gray-500 pr-1 pt-2 w-full h-full">
-                            <div class="my-0.5 mx-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-                                </svg>
-                            </div>
+            <form class="" @submit.prevent="submit">
+                    <div class="flex">
+                        <div class="">
+                            <input id="email" v-model="form.email" placeholder="Your e-mail address" />
+                        </div>
+                        <div class="w-3/12 pl-6">
+                            <button type="submit" :disabled="form.processing" class=" pb-3 pr-2 flex justify-center font-ropa text-transform: uppercase text-white bg-gray-500 pr-1 pt-2 p-1">
+                                <div class="mx-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope mt-1" viewBox="0 0 16 16">
+                                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                                    </svg>
+                                </div>
 
-                            <h1>Sign up</h1>
+                                <h1 class="pt-1">Sign up</h1>
 
-                        </button>
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div class="mt-1">
-                    <label>
-                        <input type="checkbox" name="agree">
-                        I have read and agree with the <a href="/">
+                    <label class="mt-2">
+                        <input type="checkbox" id="terms" v-model="form.terms" name="agree">
+                        I have read and agree with the <Link :href="('/terms')">
                         Confidentiality agreement and HTTP cookies
-                    </a>
+                    </Link>
                     </label>
-                </div>
-            </div>
+                </form>
         </div>
 </template>
 
