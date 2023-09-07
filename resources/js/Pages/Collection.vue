2cbebe1927fb621ebdad46646ce80ec575a94242
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-07 21:34:00 +0200

    Update Collection.vue

--- resources/js/Pages/Collection.vue
+++ resources/js/Pages/Collection.vue
@@ -58,7 +58,7 @@
                         <div class="flex">
                             <input v-model="vMin" type="number" class="w-20 [text:textfield] [&::-webkit-outer-spin-button]:text-white [&::-webkit-inner-spin-button]:text-white" />
                             <div class="flex text-center">Price in Euros</div>
-                            <input v-model="vMax" type="number" class="input w-20"  />
+                            <input v-model="vMax" type="number" class="w-20"  />
                         </div>
                     </div>
 
