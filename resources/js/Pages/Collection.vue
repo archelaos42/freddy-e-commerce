09f416ae7d6d3b46732e541c08bb40d026c136eb
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-09 21:24:46 +0200

    Update Collection.vue

--- resources/js/Pages/Collection.vue
+++ resources/js/Pages/Collection.vue
@@ -22,7 +22,7 @@
                     <h1 class="font-bold font-ropa text-transform: uppercase text-tra text-base">Filters</h1>
                     <div class="relative bottom-3">________</div>
                 </div>
-                <Link class="flex my-auto text-sm py-1 px-2" v-bind:href="'/collection/' + collection.id">
+                <Link class="flex my-auto text-sm py-1 px-2" v-bind:href="'/collection/' + collection.id" preserve-scroll>
                     <button class="flex my-auto font-ropa text-sm py-1 px-2 mb-3 text-white bg-black">
                         <svg class="fill-white my-auto mr-1" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" width="12" height="12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404 0-.75.324-.75.749 0 .193.073.384.219.531l2.722 2.722-2.728 2.728c-.147.147-.22.34-.22.531 0 .427.35.75.751.75.192 0 .384-.073.53-.219l2.728-2.728 2.729 2.728c.146.146.338.219.53.219.401 0 .75-.323.75-.75 0-.191-.073-.384-.22-.531l-2.727-2.728 2.717-2.717c.146-.147.219-.338.219-.531 0-.425-.346-.75-.75-.75-.192 0-.385.073-.531.22z" fill-rule="nonzero"/></svg>
                         <h1>Clear</h1>
