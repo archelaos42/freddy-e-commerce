ab1c26a3fd2dc150bb5284c05761de31a3c9a1d5
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2023-09-09 20:43:09 +0200

    Update Collection.vue

--- resources/js/Pages/Collection.vue
+++ resources/js/Pages/Collection.vue
@@ -56,7 +56,7 @@
                             class="slider"
                         />
                         <div class="flex">
-                            <input v-model="vMin" type="number" class="w-14 mt-3 input [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none " />
+                            <input v-model="vMin" type="number" class="w-14 mt-1 input [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none " />
                             <div class="my-auto mx-1">
                             <div>
                                 <button @click="vMin += 1 "><svg width="12" height="12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"/>
@@ -68,7 +68,7 @@
                             </div>
                         </div>
                             <div class="flex text-center">Price in Euros</div>
-                            <input v-model="vMax" type="number" class="w-14 mt-3 input [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none " />
+                            <input v-model="vMax" type="number" class="w-14 mt-1 input [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none " />
                             <div class="my-auto mx-1">
                             <div>
                                 <button @click="vMax += 1 "><svg width="12" height="12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"/>
