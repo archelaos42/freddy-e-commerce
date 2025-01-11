15c2d554fbbd610033b4a3b63cbff92262ddd29e
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2025-01-11 02:51:24 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'award_points' => 0,
-            'password' => 'password',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'is_admin' => 1,
         ]);
 
