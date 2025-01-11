04bb60188e5c55893a28194fac9bf57af7835281
archelaos42 <89926371+archelaos42@users.noreply.github.com>
2025-01-11 23:40:12 +0100

    Update FortifyServiceProvider.php

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -32,7 +32,7 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        URL::forceScheme('http');
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
